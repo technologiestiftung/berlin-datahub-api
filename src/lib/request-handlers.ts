af4b849e47a0a32d722cbb4fa8c55f9596b688d8
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-09-29 16:53:23 +0200

    fix lost prisma folder and fix get/post device

--- src/lib/request-handlers.ts
+++ src/lib/request-handlers.ts
@@ -1,5 +1,5 @@
 import createError from "http-errors";
-import { PrismaClient, Application, Device, Record } from "@prisma/client";
+import { PrismaClient, Device, Record } from "@prisma/client";
 import { Handler, Request, response, Response } from "express";
 import _ from "lodash";
 import { createPayload } from "./utils";
@@ -7,61 +7,63 @@ const prisma = new PrismaClient();
 
 type HandlerFunction = (request: Request, response: Response) => Promise<void>;
 
-export const getApplications: HandlerFunction = async (_request, response) => {
-  const applications = await prisma.application.findMany();
-  response.json(createPayload({ applications }));
-};
+// export const getApplications: HandlerFunction = async (_request, response) => {
+//   const applications = await prisma.application.findMany();
+//   response.json(createPayload({ applications }));
+// };
 
-export const postApplication: HandlerFunction = async (request, response) => {
-  console.log(request.body);
-  const { name, applicationKey, description } = request.body;
-  if (!name || typeof name !== "string") {
-    throw createError(400, `application name is not defined or not a string`);
-  }
-  if (!applicationKey || typeof applicationKey !== "string") {
-    throw createError(
-      400,
-      `application applicationkey is not defined or not a string`,
-    );
-  }
+// export const postApplication: HandlerFunction = async (request, response) => {
+//   console.log(request.body);
+//   const { name, applicationKey, description } = request.body;
+//   if (!name || typeof name !== "string") {
+//     throw createError(400, `application name is not defined or not a string`);
+//   }
+//   if (!applicationKey || typeof applicationKey !== "string") {
+//     throw createError(
+//       400,
+//       `application applicationkey is not defined or not a string`,
+//     );
+//   }
 
-  // if (!description && typeof description !== "string") {
-  //   throw createError(400, `description is not a string`);
-  // }
+//   // if (!description && typeof description !== "string") {
+//   //   throw createError(400, `description is not a string`);
+//   // }
 
-  const application = await prisma.application.create({
-    data: { name, applicationKey, description },
-  });
-  response.status(201).json(createPayload({ application }));
-};
+//   const application = await prisma.application.create({
+//     data: { name, applicationKey, description },
+//   });
+//   response.status(201).json(createPayload({ application }));
+// };
 
-export const getApplicationById: HandlerFunction = async (
-  _request,
-  response,
-) => {
-  const application = response.locals.application as Application;
-  response.json(createPayload({ application }));
-};
+// export const getApplicationById: HandlerFunction = async (
+//   _request,
+//   response,
+// ) => {
+//   const application = response.locals.application as Application;
+//   response.json(createPayload({ application }));
+// };
 
 export const getDevices: HandlerFunction = async (_request, response) => {
-  const application = response.locals.application as Application;
-  const devices = await prisma.device.findMany({
-    where: { applicationId: application.id },
-  });
+  // const application = response.locals.application as Application;
+  const devices = await prisma.device.findMany();
   response.json(createPayload({ devices }));
 };
 
 export const postDevice: Handler = async (request, response) => {
-  const application = response.locals.application as Application;
-  const { name, description } = request.body;
-  if (!name || typeof name !== "string") {
-    throw createError(400, `device name is not defined or not a string`);
+  // const application = response.locals.application as Application;
+  const { description, ttnDeviceId, ttnAppId } = request.body;
+  if (!ttnDeviceId || typeof name !== "string") {
+    throw createError(400, `device ttnDeviceId is not defined or not a string`);
   }
+  if (!ttnAppId || typeof name !== "string") {
+    throw createError(400, `device ttnAppId is not defined or not a string`);
+  }
+
   const device = await prisma.device.create({
     data: {
-      name,
+      ttnDeviceId,
+      ttnAppId,
       description,
-      Application: { connect: { id: application.id } },
     },
   });
   response.status(201).json(createPayload({ device }));
