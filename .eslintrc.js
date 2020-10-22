5a5d0ffd93200211cef2c826ab606d80e6153e85
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-10-22 15:50:32 +0200

    setup testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,9 @@
-module.exports = require("@inpyjamas/scripts/eslint");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const localConfig = {
+  rules: {
+    "jest/no-hooks": "off",
+  },
+};
+const utlities = require("@inpyjamas/scripts/dist/utlities");
+const config = require("@inpyjamas/scripts/eslint");
+module.exports = utlities.merge(config, localConfig);
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path");
 const utlities = require("@inpyjamas/scripts/dist/utlities");
 const config = require("@inpyjamas/scripts/jest");
 module.exports = utlities.merge(config, {
+  testEnvironment: path.join(__dirname, "prisma", "prisma-test-environment.js"),
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 });
