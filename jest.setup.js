a97fae8efc0ef54aaa155a8b42fd749296e7aba9
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-10-26 14:22:53 +0100

    fix(tests): mock winston

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,22 @@
 jest.setTimeout(10000);
 process.env.APP_SECRET = "superdupersecret";
+const logger = {
+  debug: jest.fn(),
+  log: jest.fn(),
+  add: jest.fn(),
+};
+
+jest.mock("winston", () => ({
+  format: {
+    colorize: jest.fn(),
+    combine: jest.fn(),
+    label: jest.fn(),
+    timestamp: jest.fn(),
+    printf: jest.fn(),
+    simple: jest.fn(),
+  },
+  createLogger: jest.fn().mockReturnValue(logger),
+  transports: {
+    Console: jest.fn(),
+  },
+}));
