bfb72333539922a86ca36a6b1092d964daa9d8c8
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-10-29 13:05:19 +0100

    chore(envs): Check for existing envs

--- src/lib/envs.ts
+++ src/lib/envs.ts
@@ -1,5 +1,22 @@
+/**
+ * To make sure the needed environment variables are set we assert here on them and throw errors when they are not present.
+ *
+ */
+
 if (process.env.APP_SECRET === undefined) {
-  throw new Error("APP_SECRET is not defined");
+  throw new Error("Environment varibale 'APP_SECRET' is not defined");
 }
+if (process.env.LOGDNA_KEY === undefined) {
+  throw new Error("Environment varibale 'LOGDNA_KEY' is not defined");
+}
+if (process.env.NODE_ENV === undefined) {
+  throw new Error("Environment varibale 'NODE_ENV' is not defined");
+}
+
 const APP_SECRET = process.env.APP_SECRET;
-export { APP_SECRET };
+const LOGDNA_KEY = process.env.LOGDNA_KEY;
+const NODE_ENV = process.env.NODE_ENV;
+/**
+ * export the env variables
+ */
+export { APP_SECRET, LOGDNA_KEY, NODE_ENV };
