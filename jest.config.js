0e9e0d9a2cc9e5d88af5cb7bd5410790be486b98
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-11-02 16:17:00 +0100

    test(coverage): reduce coverage

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,14 @@ const config = require("@inpyjamas/scripts/jest");
 module.exports = utlities.merge(config, {
   testEnvironment: path.join(__dirname, "prisma", "prisma-test-environment.js"),
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  coverageThreshold: {
+    global: {
+      branches: 65,
+      functions: 70,
+      lines: 75,
+      statements: 75,
+    },
+  },
   collectCoverageFrom: [
     "src/**/*.{ts,ts}",
     "!src/index.ts",
