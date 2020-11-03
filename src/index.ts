a947ceb707edaf716e945a84c805591577e33b26
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-11-03 07:48:56 +0100

    add eslint to pipeline

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { server } from "./server";
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+  process.stdout.write(`Listening on http://localhost:${PORT}\n`);
 });
