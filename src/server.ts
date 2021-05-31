af9a6b5e038baf9578289138c7091865db47de05
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-05-31 17:22:10 +0200

    fix(logdna): Remove logdna
    This project will be archived so we don't need to fix this issue now

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV !== "test") {
 }
 
 server.get(
-  ["/", "healthcheck", "/api/healthcheck", "/api/v1/healthcheck"],
+  ["/", "/healthcheck", "/api/healthcheck", "/api/v1/healthcheck"],
   generalLimiter,
   asyncWrapper(status),
 );
